2ef7f37a2af7ddd7e95d1a03afa5db1962cef27e
null4848 <seungmin0055@gmail.com>
2025-04-01 10:03:50 +0900

    좌표맞추기

---
+++ py0331/p0331_06.py
@@ -0,0 +1,11 @@
+# x 좌표: 1
+# y 좌표: 3
+# 1, 3 -> 문자 인식
+
+# num1 = int(input("X좌표 : "))
+# num2 = int(input("Y좌표 : "))
+# print(f"[X,Y 좌표 : {num1},{num2} ]")
+
+num = input("X,Y 좌표 (0,0) : ") # 1, 3
+n_list = num.split(" ")
+print(f"[ X,Y 좌표 : {n_list} ]")
